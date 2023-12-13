dd2d4ef0767e0ded14064f996041e6455d120851
EduardoYujraT <eduardo.yujra@snoopconsulting.com>
2023-12-13 19:28:00 -0300

    Agregar archivos de proyecto.

---
+++ Cliente/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+
+
+namespace PROE
+{
+    internal class Cliente
+    {
+        private IPHostEntry? clienteHost;
+        private IPAddress? clienteIP;
+        private IPEndPoint? endCliente;
+        private Socket? Scliente;
+        public Cliente (int puerto)
+        {
+            this.clienteHost = Dns.GetHostEntry("localHost");
+            this.clienteIP = this.clienteHost.AddressList[0];
+            this.endCliente = new IPEndPoint(this.clienteIP, puerto);
+            this.Scliente = new Socket(this.clienteIP.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            Scliente.Connect(this.endCliente);//eqwerdf
+        }
+
+        public void enviarTexto()
+        {
+            string msgEnviado = clienteIP + " " + Console.ReadLine() + " <%n%>";
+            byte[] buffer = ASCIIEncoding.ASCII.GetBytes(msgEnviado);
+            Scliente.Send(buffer);
+
+        }
+        public void recivirTexto()
+        {
+            byte[] buffer = new byte[1024];
+            Scliente.Receive(buffer);
+            string msgRecivido = ASCIIEncoding.ASCII.GetString(buffer);
+            Console.WriteLine(msgRecivido);
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Cliente/Program.cs
@@ -0,0 +1,66 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using PROE;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+
+        Cliente cliente = new Cliente(4404);
+
+        while (true)
+        {
+            Console.WriteLine("Inserte su mensaje");
+            cliente.enviarTexto();
+            cliente.recivirTexto();
+            Console.WriteLine("Salir: Y/..");
+            if (Console.ReadLine() == "Y") break;
+        }
+
+        Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //Socket listen = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        //IPEndPoint connect = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 31255);
+
+
+        //listen.Connect(connect);
+
+        //byte[] envianInfo = new byte[100];
+        //string mensaje;
+
+        //Console.WriteLine("Ingrese el mensaje a enviar: ");
+        //mensaje = Console.ReadLine();
+
+        //envianInfo = Encoding.Default.GetBytes(mensaje);
+        //listen.Send(envianInfo);
+
+
+        //Console.ReadKey();
+
+    }
+}
